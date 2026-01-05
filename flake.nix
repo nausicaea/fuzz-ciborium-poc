81e9f148a58221d5a7f6a225e3978957b5fb7b98
Eleanor Young <developer@nausicaea.net>
2026-01-05 14:56:14 +0100

    chore: add the nix flake and direnv file for the rust development environment

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          config.allowUnfree = true;
+        };
+
+        rustEnv = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+          extensions = [ "rust-src" "clippy" "rustfmt" "llvm-tools" ];
+        });
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustEnv
+          ];
+
+          buildInputs = with pkgs; [
+            # openssl
+            # pkg-config
+            # git
+            # trufflehog
+            # pre-commit
+            rust-analyzer
+            cargo-fuzz
+          ];
+
+          RUST_BACKTRACE = "full";
+        };
+      }
+    );
+}
+
